854eb4549ef19cd0e5bd0bc2ac30167ac7e8c561
HaiwenZhang <haiwensummer@gmail.com>
2021-10-07 10:58:26 +0800

    Add basic Trianer class

--- utils.py
+++ utils.py
@@ -6,11 +6,6 @@ import numpy as np
 import torch
 import torch.distributed as dist
 
-try:
-    # noinspection PyUnresolvedReferences
-    from apex import amp
-except ImportError:
-    amp = None
 
 
 def load_checkpoint(config, model, optimizer, lr_scheduler, logger):
@@ -30,7 +25,6 @@ def load_checkpoint(config, model, optimizer, lr_scheduler, logger):
         config.TRAIN.START_EPOCH = checkpoint['epoch'] + 1
         config.freeze()
         if 'amp' in checkpoint and config.AMP_OPT_LEVEL != "O0" and checkpoint['config'].AMP_OPT_LEVEL != "O0":
-            amp.load_state_dict(checkpoint['amp'])
         logger.info(f"=> loaded successfully '{config.MODEL.RESUME}' (epoch {checkpoint['epoch']})")
         if 'max_accuracy' in checkpoint:
             max_accuracy = checkpoint['max_accuracy']
@@ -47,8 +41,6 @@ def save_checkpoint(config, epoch, model, max_accuracy, optimizer, lr_scheduler,
                   'max_accuracy': max_accuracy,
                   'epoch': epoch,
                   'config': config}
-    if config.AMP_OPT_LEVEL != "O0":
-        save_state['amp'] = amp.state_dict()
 
     save_path = os.path.join(config.OUTPUT, f'ckpt_epoch_{epoch}.pth')
     logger.info(f"{save_path} saving......")
